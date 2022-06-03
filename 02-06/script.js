812b74805e4da3ff8d35dc381e6573bfdd972b95
Alex-Kant <alex18popov@gmail.com>
2022-06-03 23:35:12 +0600

    feat : 02-06

---
+++ 02-06/script.js
@@ -0,0 +1,13 @@
+//1)
+let arr = ['a','b', 'c','d'];
+console.log(arr[0],'+',arr[1] ,arr[2],'+',arr[3])
+
+//2)
+let mun = ['10','15','9','10'];
+let result = (mun[0]*mun[1] + mun[2]*mun[3])
+console.log(result)
+
+//3)
+let age = ['a','b', 'c']
+age.push('1','2','3');
+console.log(age);
